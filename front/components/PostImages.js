29b038bd897996795ac4fcb0cd32bf039e0e4a19
y2gcoder <y2gcoder@gmail.com>
2021-02-08 13:23:34 +0900

    feat: change backUrl from front

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
 
 import ImagesZoom from './ImagesZoom';
+import { backUrl } from '../config/config';
 
 function PostImages({ images }) {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -15,7 +16,7 @@ function PostImages({ images }) {
   if (images.length === 1) {
     return (
       <>
-        <img role="presentation" src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -23,8 +24,8 @@ function PostImages({ images }) {
   if (images.length === 2) {
     return (
       <>
-        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`http://localhost:3065/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -32,7 +33,7 @@ function PostImages({ images }) {
   return (
     <>
       <div>
-        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ display: 'inline-block', width: '50%' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         <div
           role="presentation"
           style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
