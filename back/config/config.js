5f2522b8eaf6e76400cfdd42b2c201753034ed94
y2gcoder <y2gcoder@gmail.com>
2021-02-08 11:18:15 +0900

    refactor : change mysql user root to nodebird

--- back/config/config.js
+++ back/config/config.js
@@ -11,14 +11,14 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
+    "username": "nodebird",
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird-test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
+    "username": "nodebird",
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
